90f2f3b68fc47e3fc94e046c020699493a88190b
konoec <kono.caec@gmail.com>
2025-07-10 17:07:34 -0500

    XDDDDDDDd

--- src/Shared/DTO/generic/BaseResponseDTO.php
+++ src/Shared/DTO/generic/BaseResponseDTO.php
@@ -3,8 +3,9 @@
 namespace itaxcix\Shared\DTO\generic;
 
 use DateTimeImmutable;
+use JsonSerializable;
 
-readonly class BaseResponseDTO {
+readonly class BaseResponseDTO implements JsonSerializable {
     public function __construct(
         public bool $success,
         public ?string $message = null,
@@ -12,4 +13,15 @@ readonly class BaseResponseDTO {
         public array|null $error = null,
         public DateTimeImmutable $timestamp = new DateTimeImmutable()
     ) {}
+
+    public function jsonSerialize(): array
+    {
+        return [
+            'success' => $this->success,
+            'message' => $this->message,
+            'data' => $this->data,
+            'error' => $this->error,
+            'timestamp' => $this->timestamp
+        ];
+    }
 }
